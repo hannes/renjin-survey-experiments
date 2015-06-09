4ffd1fd835c6a34dd36fde68a51d48d43d436b7f
Hannes Muehleisen <hannes@muehleisen.org>
2015-06-09 16:23:59 +0200

    mini example

---
+++ mini-defer.R
@@ -0,0 +1,17 @@
+svydata <- readRDS("alabama.rds")
+
+wts <- svydata[,200:279]
+x <- svydata$agep
+dim(x) <- c(length(x), 1)
+pw <- 1L
+
+print(system.time({
+	repmeans<-matrix(ncol=NCOL(x), nrow=ncol(wts))
+	for(i in 1:ncol(wts)){
+	  repmeans[i,]<-t(colSums(wts[,i]*x*pw)/sum(pw*wts[,i]))
+	}
+}))
+
+print(system.time({
+	print(repmeans)
+}))
